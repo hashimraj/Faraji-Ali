726037647321386cc253180cb160be1bdf09b585
Raj <hashimraj02@gmail.com>
2025-08-06 00:37:55 +0300

    portfolio modifications

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import "./portfolio.css";
 import IMG1 from "../../assets/portfolio1.PNG";
 import IMG2 from "../../assets/portfolio2.PNG";
-import IMG3 from "../../assets/portfolio3.PNG"
+import IMG3 from "../../assets/portfolio3.PNG";
+import IMG4 from "../../assets/portfolio4.png";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.png";
 
 const projectData = [
   {
@@ -15,8 +18,8 @@ const projectData = [
   },
   {
     id: 2,
-    image: IMG2,
-    title: "Charts Templates & Infographics in Figma",
+    image: IMG4,
+    title: "Infographics in Figma",
     github: "https://github.com",
     demo: "https://twitter-clone-2npx.onrender.com",
     category: "design",
@@ -24,31 +27,31 @@ const projectData = [
   {
     id: 3,
     image: IMG3,
-    title: "Figma Dashboard UI Kit for Data Design Web Apps",
+    title: "Figma Dashboard UI Kit",
     github: "https://github.com",
     demo: "https://twitter-clone-2npx.onrender.com",
     category: "web",
   },
   {
     id: 4,
-    image: IMG1,
-    title: "Maintaining Tasks and Tracking Progress",
+    image: IMG5,
+    title: "Maintaining Tasks App",
     github: "https://github.com",
     demo: "https://twitter-clone-2npx.onrender.com",
-    category: "mobile",
+    category: "design",
   },
   {
     id: 5,
-    image: IMG1,
-    title: "Crypto Currency Dashboard & Financial Visualization",
+    image: IMG2,
+    title: "Crypto Currency Dashboard",
     github: "https://github.com",
     demo: "https://twitter-clone-2npx.onrender.com",
     category: "web",
   },
   {
     id: 6,
-    image: IMG2,
-    title: "Crypto Currency Dashboard & Financial Visualization",
+    image: IMG6,
+    title: "Financial Visualization Dashboard",
     github: "https://github.com",
     demo: "https://twitter-clone-2npx.onrender.com",
     category: "design",
@@ -58,21 +61,41 @@ const projectData = [
 const Portfolio = () => {
   const [filter, setFilter] = useState("all");
 
-  const filteredProjects = 
-  filter === "all"
-  ? projectData
-  : projectData.filter((project) => project.category === filter);
+  const filteredProjects =
+    filter === "all"
+      ? projectData
+      : projectData.filter((project) => project.category === filter);
 
-    return (
+  return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="filter-buttons">
-        <button onClick={() => setFilter("all")} className={filter === "all" ? "active" : ""}>All</button>
-        <button onClick={() => setFilter("web")} className={filter === "web" ? "active" : ""}>Web Apps</button>
-        <button onClick={() => setFilter("mobile")} className={filter === "mobile" ? "active" : ""}>Mobile Apps</button>
-        <button onClick={() => setFilter("design")} className={filter === "design" ? "active" : ""}>Design</button>
+        <button
+          onClick={() => setFilter("all")}
+          className={filter === "all" ? "active" : ""}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setFilter("web")}
+          className={filter === "web" ? "active" : ""}
+        >
+          Web Apps
+        </button>
+        <button
+          onClick={() => setFilter("mobile")}
+          className={filter === "mobile" ? "active" : ""}
+        >
+          Mobile Apps
+        </button>
+        <button
+          onClick={() => setFilter("design")}
+          className={filter === "design" ? "active" : ""}
+        >
+          Design
+        </button>
       </div>
 
       <div className="container portfolio__container">
@@ -82,14 +105,29 @@ const Portfolio = () => {
               <div className="portfolio__item-image">
                 <img src={image} alt={title} />
               </div>
-              <h3>{title}</h3>
-              <div className="portfolio__item-cta">
-                <a href={github} className="btn" target="_blank">
-                  Github
-                </a>
-                <a href={demo} className="btn btn-primary" target="_blank">
-                  Visit App 
-                </a>
+              <div className="portfolio-detail">
+                <h5>{title}</h5>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn" target="_blank">
+                    Github
+                  </a>
+                  <a href={demo} className="btn btn-primary" target="_blank">
+                    Visit
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        d="M5 19L19 5M19 5H9M19 5V15"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </a>
+                </div>
               </div>
             </article>
           );
